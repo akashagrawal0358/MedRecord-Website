eb8fac59da78ceadcd3839b5858bca06e78c6404
Akash Agrawal <akashagrawal0358@gmail.com>
2023-11-11 20:19:20 +0530

    Working start on layout Patient- profile page

--- client/src/pages/PatientProfile.js
+++ client/src/pages/PatientProfile.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import '../styles/Patient-Profile.css'
+
 
 const PatientProfile = () => {
   return (
-    <div className='container' >
-      <div className='container-profile'>
-        PatientProfile
+   // <div className='container' >
+      <div className='container-outer-profile'>
+        <div className='container-profile'>
+          PatientProfile
+        </div>
       </div>
-    </div>
+  //  </div>
   )
 }
 
