f495e93422ca4040ed64bb4b1709d47c41947872
Akash Agrawal <akashagrawal0358@gmail.com>
2023-11-25 20:28:16 +0530

    Backend Setup

---
+++ Server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const PORT = process.env.PORT || 5000 ;
+const app = express() ;
+
+app.use(cors) ;
+
+
+
+
+
+
+
+app.listen( PORT , ()=>{
+    console.log(`Server is running at : ${PORT}` );
+})
+
